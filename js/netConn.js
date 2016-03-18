3f9d3d0d3c03736962f39ae25f8fa5c3ff0489c3
Rocky <rockyluoqi1215@gmail.com>
2016-03-18 21:38:22 +0800

    add gallery html and Unite gallery plugin

---
+++ js/netConn.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Luoqi on 3/18/2016.
+ * This js is designed for net communication and data transmission.
+ */
+;(function(window) {
+
+    var coord = {
+        x:x,
+        y:y
+    }
+
+    function getImages(){
+
+    }
+
+    function sendCoords(coordArray) {
+
+    }
+})(window);
