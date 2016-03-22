b17878f13df29bfaf40c60e09b439000fe8ad0ee
Rocky <rockyluoqi1215@gmail.com>
2016-03-22 18:27:40 +0800

    parse json to object and draw on the context

--- js/editorMain.js
+++ js/editorMain.js
@@ -374,8 +374,8 @@
                     drawStraightLine(mouseX, mouseY, context3);
                 }
 
-                //console.log(num_of_click);
-                if (distance(point, mouse) < 3 && num_of_click > 3) {
+                //console.log(num_of_click);s
+                if (distance(point, mouse) < 15 && num_of_click > 3) {
                     //console.log("closed is true");
                     point.closed2 = true;
                 } else {
@@ -874,4 +874,156 @@
         }
         $MenuItem.removeClass("menu-open");
     });
+
+
+    document.getElementById('successBtn').addEventListener('click',drawNewImage);
+
+    var name,url,description;
+
+    function drawNewImage(){
+        var data = localStorage['obstacle'];
+        var obj = JSON.parse(data);
+
+        name = obj.mapName;
+        url = obj.url;
+        description = obj.description;
+        console.log(name +" "+url+" "+description);
+
+        var tempArray;
+
+        /**
+         * parse lines
+         */
+        var lines = [];
+         tempArray = obj.obstacles.lines;
+        for(var i = 0;i<tempArray.length;i++) {
+            //console.log(tempArray[i].start.x+" "+tempArray[i].start.y)
+            var startPot = {"x":0,"y":0};
+            startPot.x = tempArray[i].start.x;
+            startPot.y = tempArray[i].start.y;
+            var endPot = {"x":0,"y":0};
+            endPot.x = tempArray[i].end.x;
+            endPot.y = tempArray[i].end.y;
+            var line = {
+                "start":startPot,
+                "end":endPot
+            };
+            line.start = startPot;
+            line.end = endPot;
+
+            lines.push(line);
+        }
+
+        //for(var i = 0;i<lines.length;i++) {
+        //    console.log(lines[i].start.x+ " " + lines[i].start.y);
+        //    console.log(lines[i].end.x+ " " + lines[i].end.y);
+        //}
+
+        drawLineObstacle(lines,context);
+
+        /**
+         * parse rectangle
+         */
+        var rectangles = [];
+        tempArray = obj.obstacles.rectangles;
+        for(var i = 0;i<tempArray.length;i++) {
+            //console.log(tempArray[i].start.x+" "+tempArray[i].start.y)
+            var startPot = {"x":0,"y":0};
+            startPot.x = tempArray[i].start.x;
+            startPot.y = tempArray[i].start.y;
+            var endPot = {"x":0,"y":0};
+            endPot.x = tempArray[i].end.x;
+            endPot.y = tempArray[i].end.y;
+            var rectangle = {
+                "start":startPot,
+                "end":endPot
+            };
+            rectangle.start = startPot;
+            rectangle.end = endPot;
+
+            rectangles.push(rectangle);
+        }
+
+        //console.log(rectangles.length);
+        //
+        //
+        //for(var i = 0;i<rectangles.length;i++) {
+        //    console.log(rectangles[i].start.x+ " " + rectangles[i].start.y);
+        //    console.log(rectangles[i].end.x+ " " + rectangles[i].end.y);
+        //}
+
+        drawRectangleObstacle(rectangles,context);
+
+        /**
+         * parse polygon
+         */
+        var polygons = [];
+        tempArray = obj.obstacles.polygons;
+        var array;
+        for(var j = 0;j<tempArray.length;j++) {
+            var polygon = [];
+            for (var i = 0; i < tempArray[j].length; i++) {
+                array = tempArray[j];
+                var pot = {"x": 0, "y": 0};
+                pot.x = array[i].x;
+                pot.y = array[i].y;
+
+                polygon.push(pot);
+            }
+            polygons.push(polygon);
+        }
+
+        drawPolygonObstacle(polygons,context);
+
+    }
+
+    function drawLineObstacle(lines,contextT) {
+        for(var i = 0;i<lines.length;i++) {
+            contextT.beginPath();
+            contextT.lineCap = "round";
+            //contextT.lineCap = chosenWidth;
+            contextT.strokeStyle = $colorItem.css("background-color");
+            contextT.moveTo(lines[i].start.x, lines[i].start.y);
+            contextT.lineTo(lines[i].end.x, lines[i].end.y);
+            contextT.stroke();
+        }
+    }
+
+    function drawRectangleObstacle(rectangles,contextT) {
+        for(var i = 0;i<rectangles.length;i++) {
+            contextT.beginPath();
+            context.lineCap = "round";
+            contextT.strokeStyle = $colorItem.css("background-color");
+            //is fill color rect
+            //contextT.fillRect(startX,startY,toX-startX,toY-startY);
+            contextT.strokeRect(rectangles[i].start.x,rectangles[i].start.y ,
+                rectangles[i].end.x- rectangles[i].start.x,
+                rectangles[i].end.y- rectangles[i].start.y);
+        }
+    }
+
+    function drawPolygonObstacle(polygons,contextT) {
+        for(var i = 0;i < polygons.length;i++) {
+            var polygon = polygons[i];
+            contextT.beginPath();
+            contextT.strokeStyle = $colorItem.css("background-color");
+            for (var j = 0; j < polygon.length; j++) {
+                if (j + 1 === polygon.length) {
+                    contextT.moveTo(polygon[j].x, polygon[j].y);
+                    contextT.lineTo(polygon[0].x, polygon[0].y);
+                    contextT.stroke();
+                } else {
+                    contextT.moveTo(polygon[j].x, polygon[j].y);
+                    //console.log(polygon[j].x + " " + polygon[j].y)
+                    contextT.lineTo(polygon[j + 1].x, polygon[j + 1].y);
+                    //console.log(polygon[j + 1].x + " " + polygon[j + 1].y)
+                    contextT.stroke();
+                }
+
+            }
+        }
+    }
+
+
+
 })(window);
--- js/imageHandle.js
+++ js/imageHandle.js
@@ -31,6 +31,12 @@ function drawNewImage(img,w,h) {
     }
 }
 
+var imageData = {
+    width:0,
+    height:0,
+
+};
+
 //设置缩略图路径
 function setThumbImg(img){
     //直接设置缩略图元素的image-url
@@ -39,13 +45,46 @@ function setThumbImg(img){
 function setImage() {
     var imgUrl = $(".ug-thumb-selected img").get(0).src;
 
-    //切字符,机智
+    //切字符,个位数和非个位数
     var wordArray = imgUrl.split("/");
     var num = wordArray[7].charAt(5);
+    var num2 = wordArray[7].charAt(6);
+    num = parseInt(num);
+    num2 = parseInt(num2);
+    if(typeof num2 ==='number' && !isNaN(num2)) {
+        num = num * 10 + num2;
+        //console.log(num);
+    }
+
     //跨文件传参常用方法
-    localStorage["bgUrl"] = "/mapEditor/plugin/images/big/image"+num+".jpg";
+    if(num < 99) {
+        localStorage["bgUrl"] = "/mapEditor/plugin/images/big/image" + num + ".jpg";
+    }
+
+    //usage:
+    readTextFile("/mapEditor/asset/json/map.json", function(text){
+        localStorage["obstacle"] = text;
+        //console.log(text);
+    });
+
+
+
     window.open('editorTest3.html','_self',false);
-    console.log("setImage  "+imgUrl+ wordArray[7] + "  "+num);
+    //console.log("setImage  "+imgUrl+ wordArray[7] + "  "+num);
 }
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+
 
 
